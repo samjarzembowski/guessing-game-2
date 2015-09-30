5c38881b9c85a3a6a5620bf420285126993823ad
Roland D. San Nicolas <rdsannicolas@gmail.com>
2015-09-30 13:55:28 -0700

    updated code with error correction

---
+++ game.js
@@ -0,0 +1,32 @@
+// Question 1
+var question1 = prompt('Does Roland live in Washington State?').toLowerCase();
+console.log('Question 1 response: ' + question1);
+
+if ((question1 === 'yes') || (question1 === 'y')) {
+  alert("You are correct!");
+} else {
+  alert("You are not correct!");
+}
+
+
+// Question 2
+var question2 = prompt('Is Roland a Musician?').toLowerCase();
+console.log('Question 2 response: ' + question2);
+
+if ((question2 === 'yes') || (question2 === 'y')) {
+  alert("You are correct!");
+} else {
+  alert("You are not correct!");
+}
+
+
+// Question 3
+var question3 = prompt('Is Roland a Cougar or a Husky?').toLowerCase();
+console.log('Question 3 response: ' + question3);
+
+if ((question3 === 'cougar') || (question3 === 'Cougar')) {
+  alert("You are correct!");
+} else {
+  alert("You are not correct!");
+}
+
