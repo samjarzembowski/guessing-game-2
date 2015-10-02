098097d2299596813d84ee8e940ffdbf8dbbae99
samjarzembowski <sjarzembowski@gmail.com>
2015-10-02 15:24:02 -0700

    added image

--- game.js
+++ game.js
@@ -5,7 +5,10 @@ var question2;
 var question3;
 var one = document.getElementById('one');
 var two = document.getElementById('two');
-var three = document.getElementById('three')
+var three = document.getElementById('three');
+var thumb1 = document.getElementById('thumb1');
+var thumb2 = document.getElementById('thumb2');
+var thumb3 = document.getElementById('thumb3');
 var questions = ['Does Roland live in Washington State?', 'Is Roland a Musician?', 'Is Roland a Cougar?'];
 var answers = ['YES', 'Y'];
 
@@ -14,6 +17,7 @@ function question1(){
 question1 = prompt(questions[0]).toUpperCase();
 if ((question1 === answers[0]) || (question1 === answers[1])) {
   one.innerHTML = "You are correct!";
+  thumb1.innerHTML = '<img src="img/thumb.jpg" width="200" />';
 } else {
   one.innerHTML = "You are not correct.";
 }
@@ -24,6 +28,8 @@ function question2(){
 question2 = prompt(questions[1]).toUpperCase();
 if ((question2 === answers[0]) || (question2 === answers[1])) {
   two.innerHTML = "You are correct!";
+  thumb2.innerHTML = '<img src="img/thumb.jpg" width="200" />';
+
 } else {
   two.innerHTML = "You are not correct.";
 }
@@ -31,9 +37,10 @@ if ((question2 === answers[0]) || (question2 === answers[1])) {
 
 // Ask Question 3
 function question3(){
-question3 = prompt(questions[2]);
+question3 = prompt(questions[2]).toUpperCase();
 if ((question3 === answers[0]) || (question3 === answers[1])) {
   three.innerHTML = "You are correct!";
+  thumb3.innerHTML = '<img src="img/thumb.jpg" width="200" />';
 } else {
   three.innerHTML = "You are not correct.";
 }
@@ -41,6 +48,9 @@ if ((question3 === answers[0]) || (question3 === answers[1])) {
 
 
 question1();
-question2();
-question3();
+window.setTimeout(question2, 1000);
+window.setTimeout(question3, 1000);
+
+// question2();
+// question3();
 
